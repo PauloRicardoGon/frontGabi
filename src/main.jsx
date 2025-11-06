c1727104d549caaed89bd28df5eedc8acf01a9fb
PauloRicardoGon <paulo.goncalves@canoas.rs.gov.br>
2025-11-06 09:13:20 -0300

    Refactor global styles into styles directory

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./app";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CustomerProvider } from "./contexts/CustomerContext";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
